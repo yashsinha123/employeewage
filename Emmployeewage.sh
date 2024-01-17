09619558b0855073abf9d7f792e85b1148c9600f
yashsinha123 <ys9326@srmist.edu.in>
2024-01-17 10:07:47 +0530

    code using use case statment added

--- Emmployeewage.sh
+++ Emmployeewage.sh
@@ -3,20 +3,22 @@ wageperHour=20
 fulldayHour=8
 isPresent=1
 checkAttendance=$((RANDOM%2))
-if [ $isPresent -eq $checkAttendance ]
-then
-echo "employee is present"
-else
-echo "absent"
-fi
+case $checkAttendance in
+  $isPresent)
+    echo "Employee is present"
+    dailyEmployeewage=$(($wageperHour * $fulldayHour))
+    echo "Daily Employee wage: $dailyEmployeewage"
+    ;;
 
+  *)
+    echo "Employee is absent"
+    ;;
+esac
 
-dailyEmployeewage=$(($wageperHour * $fulldayHour))
-echo $dailyEmployeewage
 
 parttimeHour=8
 Parttimetotalwage=$(($parttimeHour * $wageperHour))
-echo "partime employee is present with wage  $Parttimetotalwage "
+echo "partime employee is present with wage: $Parttimetotalwage "
 
 
 
